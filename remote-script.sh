7c09914bf34b19ad794546a682cdeb15fb6b5257
Yuval Greenfield <ubershmekel@gmail.com>
2018-08-22 16:13:33 -0700

    Stop `mali data clone` to allow data query changes in `mali run xp` without committing files

--- remote-script.sh
+++ remote-script.sh
@@ -4,7 +4,9 @@ set -x
 pip install missinglink
 
 #mali data clone 5685154290860032 --query '@version:f6f90ef73a86ab88f5dfec00433038555ad9a508 @sample:0.1 @seed:1337' --dest-folder ./data/mldx2
-mali data clone 5685154290860032 --query '@version:f6f90ef73a86ab88f5dfec00433038555ad9a508 @sample:0.05 @seed:1337' --destFolder './data/mldx2/$dir' --destFile '$name'
+#mali data clone 5685154290860032 --query '@version:f6f90ef73a86ab88f5dfec00433038555ad9a508 @sample:0.05 @seed:1337' --destFolder './data/mldx2/$dir' --destFile '$name'
+#mali data clone 5685154290860032 --query '@version:f6f90ef73a86ab88f5dfec00433038555ad9a508 @sample:1 @seed:1337' \
+#  --destFolder './data/mldx2/$dir' --destFile '$name'
 
 ls ./data/mldx2
 
