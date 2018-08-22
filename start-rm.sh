4bc60a8f5bca98b1d8e60ff6928dbc1390b9f8f5
Yuval Greenfield <ubershmekel@gmail.com>
2018-08-22 16:13:52 -0700

    Setup data with ml run xp`

--- start-rm.sh
+++ start-rm.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 set -x
 #  --data-volume 5685154290860032
-ml run xp --org yuval --project 5686084218388480 --git-repo git@github.com:ubershmekel/tmp-please-ignore.git --command '/code/remote-script.sh' --gpu
+#ml run xp --org yuval --project 5686084218388480 --git-repo git@github.com:ubershmekel/tmp-please-ignore.git --command '/code/remote-script.sh' --gpu
+
+
+ml run xp --org yuval --project 5686084218388480 \
+    --git-repo git@github.com:ubershmekel/tmp-please-ignore.git \
+    --command '/code/remote-script.sh' \
+    --gpu \
+    --data-volume 5685154290860032 \
+    --data-query '@version:f6f90ef73a86ab88f5dfec00433038555ad9a508 @sample:0.05 @seed:1337' \
+    --data-dest '/data/mldx2/$dir'
+
+
