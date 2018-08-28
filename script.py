e8be59784709c17302350464232ea8ff10461990
Yuval Greenfield <ubershmekel@gmail.com>
2018-08-28 10:59:13 -0700

    Removed all the cruft. Evaluation seems working

--- script.py
+++ script.py
@@ -29,18 +29,6 @@ missinglink_callback.set_properties(
     display_name=EXPERIMENT_NAME,
     description=EXPERIMENT_NOTE)
 
-# if platform == "linux" or platform == "linux2":
-#     # linux resource management
-#     DATA_ROOT = '/data'
-# elif platform == "darwin":
-#     # OS X
-#     pass
-# elif platform == "win32":
-#     # Windows...
-#     pass
-
-# train_data_dir = './fruits-360/Training/'
-# validation_data_dir = './fruits-360/Test/'
 train_data_dir = DATA_ROOT + '/train'
 validation_data_dir = DATA_ROOT + '/validation'
 test_data_dir = DATA_ROOT + '/test'
@@ -51,11 +39,11 @@ img_width, img_height = 224, 224
 
 batch_size = 16
 
-train_datagen = ImageDataGenerator(
-    rescale=1. / 255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True)
+train_datagen = ImageDataGenerator()
+#    rescale=1. / 255,
+#    shear_range=0.2,
+#    zoom_range=0.2,
+#    horizontal_flip=True)
 
 # Convert RGB [0, 255] to [0, 1.0]
 test_validate_datagen = ImageDataGenerator(rescale=1. / 255)
@@ -83,74 +71,21 @@ class_names_list = list(train_generator.class_indices.keys())
 test_class_names_list = list(test_generator.class_indices.keys())
 validation_class_names_list = list(validation_generator.class_indices.keys())
 
-#print(f"train classes: {len(class_names_list)}")
-#print(f"test classes: {len(test_class_names_list)}")
-#print(f"validattion classes: {len(validation_class_names_list)}")
-
-print(set(class_names_list) - set(test_class_names_list))
-
-def create_stack_bar_data(col, df):
-    aggregated = df[col].value_counts().sort_index()
-    x_values = aggregated.index.tolist()
-    y_values = aggregated.values.tolist()
-    return x_values, y_values
-
-def plot():
-    training_data = pd.DataFrame(train_generator.classes, columns=['classes'])
-    testing_data = pd.DataFrame(validation_generator.classes, columns=['classes'])
-
-    x1, y1 = create_stack_bar_data('classes', training_data)
-    #x1 = list(train_generator.class_indices.keys())
-    # TODO: is class_names_list in the right order?
-
-    trace1 = go.Bar(x=class_names_list, y=y1, opacity=0.75, name="Class Count")
-    layout = dict(height=400, width=1200, title='Class Distribution in Training Data', legend=dict(orientation="h"), 
-                    yaxis = dict(title = 'Class Count'))
-    fig = go.Figure(data=[trace1], layout=layout)
-    iplot(fig)
-
-    x1, y1 = create_stack_bar_data('classes', testing_data)
-    train_class_names = list(validation_generator.class_indices.keys())
-
-    trace1 = go.Bar(x=train_class_names, y=y1, opacity=0.75, name="Class Count")
-    layout = dict(height=400, width=1100, title='Class Distribution in Validation Data', legend=dict(orientation="h"), 
-                    yaxis = dict(title = 'Class Count'))
-    fig = go.Figure(data=[trace1], layout=layout)
-    iplot(fig)
-
-#import inception with pre-trained weights. do not include fully #connected layers
+# import inception with pre-trained weights. do not include fully #connected layers
 inception_base = applications.ResNet50(weights='imagenet', include_top=False)
-
-#print("!@#~#~!@!@#$#$@#$@#$@#$@$%^&*()(*&^%$#@#$%^&*(*&^%$#@")
-#print("Inception")
-#print("!@#~#~!@!@#$#$@#$@#$@#$@$%^&*()(*&^%$#@#$%^&*(*&^%$#@")
-#print(inception_base.summary())
-print("!@#~#~!@!@#$#$@#$@#$@#$@$%^&*()(*&^%$#@#$%^&*(*&^%$#@")
-
-# TODO: Put the fruits images in data management
-# TODO: Look at print(inception_base.summary())
-# TODO: Maybe put the resnet50 model (mali data clone)??? Maybe too advanced.
 
 # add a global spatial average pooling layer
 x = inception_base.output
 x = GlobalAveragePooling2D()(x)
+
 # add a fully-connected layer
 x = Dense(512, activation='relu')(x)
+
 # and a fully connected output/classification layer
 predictions = Dense(len(class_names_list), activation='softmax')(x)
+
 # create the full network so we can train on it
 inception_transfer_model = Model(inputs=inception_base.input, outputs=predictions)
-
-#import inception with pre-trained weights. do not include fully #connected layers
-#inception_base_vanilla = applications.ResNet50(weights=None, include_top=False)
-
-# add a global spatial average pooling layer
-#x = inception_base_vanilla.output
-#x = GlobalAveragePooling2D()(x)
-# add a fully-connected layer
-#x = Dense(512, activation='relu')(x)
-# and a fully connected output/classification layer
-#predictions = Dense(len(class_names_list), activation='softmax')(x)
 
 # create the full network so we can train on it
 inception_transfer_model.compile(loss='categorical_crossentropy',
@@ -165,29 +100,10 @@ history_pretrained = inception_transfer_model.fit_generator(
     validation_data=validation_generator,
     callbacks=[missinglink_callback])
 
-# evaluate the model
-#scores = inception_transfer_model.evaluate_generator(test_generator)
-#print("\n%s: %.2f%%" % (inception_transfer_model.metrics_names[1], scores[1]*100))
+print("Beginnging Model Evaluation")
 with missinglink_callback.test(inception_transfer_model):
     score = inception_transfer_model.evaluate_generator(test_generator, steps=len(test_generator))
     print('Test score:', score[0])
     print('Test accuracy:', score[1])
     print("Metrics: {}".format(inception_transfer_model.metrics_names))
-
-
-# # summarize history for accuracy
-# plt.plot(history_pretrained.history['val_acc'])
-# # plt.plot(history_vanilla.history['val_acc'])
-# plt.title('model accuracy')
-# plt.ylabel('accuracy')
-# plt.xlabel('epoch')
-# # plt.legend(['Pretrained', 'Vanilla'], loc='upper left')
-# plt.show()
-# # summarize history for loss
-# plt.plot(history_pretrained.history['val_loss'])
-# # plt.plot(history_vanilla.history['val_loss'])
-# plt.title('model loss')
-# plt.ylabel('loss')
-# plt.xlabel('epoch')
-# plt.show()
 
