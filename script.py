0e2e2cccc1467f006393b7f58325c34b99e15447
Yuval Greenfield <ubershmekel@gmail.com>
2018-08-30 00:16:14 -0700

    Added a model simpler than resnet

--- script.py
+++ script.py
@@ -9,9 +9,10 @@ from keras import applications
 from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
 from keras.models import Sequential, Model
-from keras.layers import Dense, GlobalAveragePooling2D
+from keras.layers import Dense, GlobalAveragePooling2D, Conv2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend
+import numpy as np
 
 # MissingLink snippet
 import missinglink
@@ -36,33 +37,36 @@ test_data_dir = DATA_ROOT + '/test'
 # Dimensions of images need to match the models we're transfer-learning from.
 # The input shape for ResNet-50 is 224 by 224 by 3 with values from 0 to 1.0
 img_width, img_height = 224, 224
+INPUT_CHANNELS = 3
+INPUT_SHAPE = (224, 224, INPUT_CHANNELS)
 
 #batch_size = 16
-batch_size = 32
-
-train_datagen = ImageDataGenerator(
-    rescale=1. / 255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True)
+batch_size = 16
 
 # Convert RGB [0, 255] to [0, 1.0]
-test_validate_datagen = ImageDataGenerator(rescale=1. / 255)
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+#    shear_range=0.2,
+#    zoom_range=0.2,
+    horizontal_flip=True)
 
 
+print("Train:")
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
     target_size=(img_height, img_width),
     batch_size=batch_size,
     class_mode='categorical')
 
-test_generator = test_validate_datagen.flow_from_directory(
+print("Test:")
+test_generator = ImageDataGenerator(rescale=1. / 255).flow_from_directory(
     test_data_dir,
     target_size=(img_height, img_width),
     batch_size=batch_size,
     class_mode='categorical')
 
-validation_generator = test_validate_datagen.flow_from_directory(
+print("Validation:")
+validation_generator = ImageDataGenerator(rescale=1. / 255).flow_from_directory(
     validation_data_dir,
     target_size=(img_height, img_width),
     batch_size=batch_size,
@@ -71,10 +75,10 @@ validation_generator = test_validate_datagen.flow_from_directory(
 class_names_list = list(train_generator.class_indices.keys())
 test_class_names_list = list(test_generator.class_indices.keys())
 validation_class_names_list = list(validation_generator.class_indices.keys())
+class_count = len(class_names_list)
 
 # Make sure class names are the same accross datasets.
 assert train_generator.class_indices == test_generator.class_indices == validation_generator.class_indices
-print(validation_generator.class_indices)
 
 # invert key-value to value-key for MissingLink class name reporting.
 index_to_name = {v: k for k, v in train_generator.class_indices.items()}
@@ -83,7 +87,7 @@ missinglink_callback.set_properties(class_mapping=index_to_name)
 def get_model():
     # import inception with pre-trained weights. do not include fully #connected layers
     #base_model = applications.ResNet50(weights='imagenet', include_top=False)
-    base_model = applications.MobileNet(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+    base_model = applications.MobileNet(weights='imagenet', include_top=False, input_shape=INPUT_SHAPE)
 
 
     # add a global spatial average pooling layer
@@ -94,19 +98,43 @@ def get_model():
     x = Dense(512, activation='relu')(x)
 
     # and a fully connected output/classification layer
-    predictions = Dense(len(class_names_list), activation='softmax')(x)
+    predictions = Dense(class_count, activation='softmax')(x)
 
     # create the full network so we can train on it
-    inception_transfer_model = Model(inputs=base_model.input, outputs=predictions)
+    transfer_learning_model = Model(inputs=base_model.input, outputs=predictions)
 
     # create the full network so we can train on it
-    inception_transfer_model.compile(loss='categorical_crossentropy',
+    transfer_learning_model.compile(loss='categorical_crossentropy',
                 optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
                 metrics=['accuracy'])
-    return inception_transfer_model
+    return transfer_learning_model
 
-model = get_model()
 
+def get_simple_model():
+    model = Sequential()
+    model.add(Dense(INPUT_CHANNELS, input_shape=INPUT_SHAPE, activation='relu'))
+    #model.add(Conv2D(32, (3, 3), input_shape=INPUT_SHAPE, activation = 'relu'))
+    #model.add(Dense(7, activation='relu'))
+    #model.add(GlobalAveragePooling2D())
+    model.add(Flatten())
+    #model.add(Dense(128))
+    model.add(Dense(class_count, activation='softmax'))
+    #model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])    
+    model.compile(loss='categorical_crossentropy', optimizer=optimizers.Adam(), metrics=['accuracy'])    
+    return model
+
+def evaluate(model):
+    print("Beginnging model evaluation of {} batches".format(len(test_generator)))
+    with missinglink_callback.test(model):
+        score = model.evaluate_generator(test_generator, steps=len(test_generator))
+        for name, score in zip(model.metrics_names, score):
+            print("Metric '{}': {}".format(name, score))
+
+#model = get_model()
+model = get_simple_model()
+model.summary()
+
+#evaluate(model)
 history_pretrained = model.fit_generator(
     train_generator,
     epochs=EPOCHS,
@@ -115,9 +143,5 @@ history_pretrained = model.fit_generator(
     validation_data=validation_generator,
     callbacks=[missinglink_callback])
 
-print("Beginnging Model Evaluation")
-with missinglink_callback.test(model):
-    score = model.evaluate_generator(test_generator, steps=len(test_generator))
-    for name, score in zip(model.metrics_names, score):
-        print("Metric '{}': {}".format(name, score))
+evaluate(model)
 
