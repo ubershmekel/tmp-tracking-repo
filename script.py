4d75c055d9132648959eb252dcb9dbaace39ad59
Yuval Greenfield <ubershmekel@gmail.com>
2018-09-04 14:22:57 -0700

    Hooked up all the hyperparameters

--- script.py
+++ script.py
@@ -5,13 +5,13 @@ Get data from https://github.com/missinglinkai/Fruit-Images-Dataset/archive/mast
 from sys import platform
 import os
 
-# Make keras initialization deterministic
+# Make neural network initialization deterministic
 import random
-random.seed(42)
 import numpy.random
-numpy.random.seed(1983)
 from tensorflow import set_random_seed
-set_random_seed(1997)
+random.seed(1983)
+numpy.random.seed(1997)
+set_random_seed(42)
 
 from keras import applications
 from keras.preprocessing.image import ImageDataGenerator
@@ -25,19 +25,46 @@ import numpy as np
 # MissingLink snippet
 import missinglink
 
-
-EXPERIMENT_NAME = os.environ.get("EXPERIMENT_NAME", "Sample")
+# ops vars
+EXPERIMENT_NAME = os.environ.get("EXPERIMENT_NAME", "")
 EXPERIMENT_NOTE = os.environ.get("EXPERIMENT_NOTE", "")
-DATA_ROOT = os.environ.get('DATA_ROOT', os.path.expanduser('~/sra/data/mldx9'))
-EPOCHS = int(os.environ.get("EPOCHS", "5"))
-
+DATA_ROOT = os.environ.get('DATA_ROOT', os.path.expanduser('~/sra/data/mldx-small'))
 OWNER_ID ='5cbb4c75-b52a-4386-af35-ce9ba735a4bb'
 PROJECT_TOKEN ='ejHztrwUToiIucAA'
+
+# Hyper paramaters
+EPOCHS = int(os.environ.get("EPOCHS", "4"))
+SIMPLE_LAYER_DIMENSIONALITY = int(os.environ.get("SIMPLE_LAYER_DIMENSIONALITY", "64"))
+SIMPLE_LAYER_COUNT = int(os.environ.get("SIMPLE_LAYER_COUNT", "0"))
+BATCH_SIZE = int(os.environ.get("BATCH_SIZE", "10"))
+OPTIMIZER = os.environ.get("OPTIMIZER", "sgd")
+LEARNING_RATE = float(os.environ.get("LEARNING_RATE", "0.01"))
+MODEL = os.environ.get("MODEL", "simple")
+
 missinglink_callback = missinglink.KerasCallback(
-    owner_id=OWNER_ID, project_token=PROJECT_TOKEN)
+    owner_id=OWNER_ID,
+    project_token=PROJECT_TOKEN)
 missinglink_callback.set_properties(
     display_name=EXPERIMENT_NAME,
     description=EXPERIMENT_NOTE)
+
+missinglink_callback.set_hyperparams(
+    SIMPLE_LAYER_DIMENSIONALITY=SIMPLE_LAYER_DIMENSIONALITY,
+    SIMPLE_LAYER_COUNT=SIMPLE_LAYER_COUNT,
+    BATCH_SIZE=BATCH_SIZE,
+    OPTIMIZER=OPTIMIZER,
+    LEARNING_RATE=LEARNING_RATE,
+    MODEL=MODEL,
+)
+
+# Note Adam defaults lr to 0.001
+#       SGD defaults lr to 0.01
+if OPTIMIZER == "adam":
+    optimizer = optimizers.Adam(lr=LEARNING_RATE)
+elif OPTIMIZER == "sgd":
+    optimizer = optimizers.SGD(lr=LEARNING_RATE, momentum=0.9)
+else:
+    raise Exception("Invalid optimizer '{}'".format(OPTIMIZER))
 
 train_data_dir = DATA_ROOT + '/train'
 validation_data_dir = DATA_ROOT + '/validation'
@@ -46,11 +73,10 @@ test_data_dir = DATA_ROOT + '/test'
 # Dimensions of images need to match the models we're transfer-learning from.
 # The input shape for ResNet-50 is 224 by 224 by 3 with values from 0 to 1.0
 img_width, img_height = 224, 224
-INPUT_CHANNELS = 3
-INPUT_SHAPE = (224, 224, INPUT_CHANNELS)
+input_channels = 3
+image_shape = (img_height, img_width)
+input_shape = (img_height, img_width, input_channels)
 
-#batch_size = 16
-batch_size = 10
 
 # Convert RGB [0, 255] to [0, 1.0]
 train_datagen = ImageDataGenerator(
@@ -60,26 +86,39 @@ train_datagen = ImageDataGenerator(
 #    horizontal_flip=True
 )
 
+test_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+#    shear_range=0.2,
+#    zoom_range=0.2,
+#    horizontal_flip=True
+)
+
+validation_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+#    shear_range=0.2,
+#    zoom_range=0.2,
+#    horizontal_flip=True
+)
 
 print("Train:")
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
-    target_size=(img_height, img_width),
-    batch_size=batch_size,
+    target_size=image_shape,
+    batch_size=BATCH_SIZE,
     class_mode='categorical')
 
 print("Test:")
-test_generator = ImageDataGenerator(rescale=1. / 255).flow_from_directory(
+test_generator = test_datagen.flow_from_directory(
     test_data_dir,
-    target_size=(img_height, img_width),
-    batch_size=batch_size,
+    target_size=image_shape,
+    batch_size=BATCH_SIZE,
     class_mode='categorical')
 
 print("Validation:")
-validation_generator = ImageDataGenerator(rescale=1. / 255, zoom_range=0.1, shear_range=0.1).flow_from_directory(
+validation_generator = validation_datagen.flow_from_directory(
     validation_data_dir,
-    target_size=(img_height, img_width),
-    batch_size=batch_size,
+    target_size=image_shape,
+    batch_size=BATCH_SIZE,
     class_mode='categorical')
 
 class_names_list = list(train_generator.class_indices.keys())
@@ -92,10 +131,14 @@ assert train_generator.class_indices == test_generator.class_indices == validati
 index_to_name = {v: k for k, v in train_generator.class_indices.items()}
 missinglink_callback.set_properties(class_mapping=index_to_name)
 
-def get_model():
+def get_transfer_model():
     # import inception with pre-trained weights. do not include fully #connected layers
-    #base_model = applications.ResNet50(weights='imagenet', include_top=False)
-    base_model = applications.MobileNet(weights='imagenet', include_top=False, input_shape=INPUT_SHAPE)
+    if MODEL == "resnet50":
+        base_model = applications.ResNet50(weights='imagenet', include_top=False)
+    elif MODEL == "mobilenet":
+        base_model = applications.MobileNet(weights='imagenet', include_top=False, input_shape=input_shape)
+    else:
+        raise Exception("Invalid model: '{}'".format(MODEL))
 
 
     # add a global spatial average pooling layer
@@ -113,37 +156,29 @@ def get_model():
 
     # create the full network so we can train on it
     transfer_learning_model.compile(loss='categorical_crossentropy',
-                optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+                optimizer=optimizer,
                 metrics=['accuracy'])
     return transfer_learning_model
 
 
 def get_simple_model():
     model = Sequential()
-    model.add(Dense(INPUT_CHANNELS, input_shape=INPUT_SHAPE, activation='relu'))
-    model.add(Conv2D(filters=4, kernel_size=(4, 4), strides=8, input_shape=INPUT_SHAPE, activation = 'relu'))
+    model.add(Dense(input_channels, input_shape=input_shape, activation='relu'))
+    model.add(Conv2D(filters=4, kernel_size=(4, 4), strides=8, input_shape=input_shape, activation = 'relu'))
     # model.add(MaxPooling2D(pool_size=(3, 3)))
-    # model.add(Conv2D(16, (3, 3), input_shape=INPUT_SHAPE, activation = 'relu'))
+    # model.add(Conv2D(16, (3, 3), input_shape=input_shape, activation = 'relu'))
     # model.add(MaxPooling2D())
     #model.add(Dense(2, activation='relu'))
     # model.add(GlobalAveragePooling2D())
     model.add(Flatten())
-    # model.add(Dense(128, activation='relu'))
-    # model.add(Dense(128, activation='relu'))
-    # model.add(Dense(128, activation='relu'))
-    # model.add(Dense(128, activation='relu'))
-    # model.add(Dense(128, activation='relu'))
-    # model.add(Dense(128))
-    # model.add(Dense(128))
-    # model.add(Dense(128))
-    # model.add(Dense(128))
-    # model.add(Dense(128))
-    #model.add(Dense(5, activation='relu'))
+
+    for _ in range(SIMPLE_LAYER_COUNT):
+        model.add(Dense(SIMPLE_LAYER_DIMENSIONALITY, activation='relu'))
+
     model.add(Dense(class_count, activation='softmax'))
     #model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])    
     model.compile(loss='categorical_crossentropy',
-        #optimizer=optimizers.Adam(),
-        optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+        optimizer=optimizer,
         metrics=['accuracy'])    
     return model
 
@@ -159,12 +194,14 @@ def evaluate(model):
     preds = model.predict(input_instances)
     def max_indices(predictions_seq):
         return (np.argmax(probs) for probs in predictions_seq)
-    print("Example predictions, vs labels:")
+    print("Example (prediction, label) pairs:")
     import pprint
     pprint.pprint(list(zip(max_indices(preds), max_indices(labels))))
 
-#model = get_model()
-model = get_simple_model()
+if MODEL == "simple":
+    model = get_simple_model()
+else:
+    model = get_transfer_model()
 model.summary()
 
 #evaluate(model)
